a6e578841844570f6b684fc4688b45c5bd9ca168
Michael Patel <michael.patel94@gmail.com>
2020-01-28 22:26:53 -0500

    Adding button listener

--- app/src/main/java/com/example/cameramp/MainActivity.java
+++ app/src/main/java/com/example/cameramp/MainActivity.java
@@ -7,7 +7,11 @@ Use existing camera application
 ///////////////////////////////////////////////////////////////////////////
 // IMPORTs
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 
 ///////////////////////////////////////////////////////////////////////////
@@ -24,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         // initialize UI widgets to programmatically control later
         takePictureButton = (Button) findViewById(R.id.takePictureButton);
+
+        // button listener
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                System.out.println("button pressed");
+            }
+        });
     }
 } // end class
