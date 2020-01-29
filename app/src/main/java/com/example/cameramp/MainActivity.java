dafda1aadc55ecf8b7edfb77f14382ad89601848
Michael Patel <michael.patel94@gmail.com>
2020-01-28 22:15:17 -0500

    Adding layout

--- app/src/main/java/com/example/cameramp/MainActivity.java
+++ app/src/main/java/com/example/cameramp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.cameramp;
 
+/*
+Use existing camera application
+ */
+
+///////////////////////////////////////////////////////////////////////////
+// IMPORTs
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
+///////////////////////////////////////////////////////////////////////////
+// MainActivity
 public class MainActivity extends AppCompatActivity {
 
+    Button takePictureButton;
+
+    // onCreate()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  // inflate UI
+
+        // initialize UI widgets to programmatically control later
+        takePictureButton = (Button) findViewById(R.id.takePictureButton);
     }
-}
+} // end class
