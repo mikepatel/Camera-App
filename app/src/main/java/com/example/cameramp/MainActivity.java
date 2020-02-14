b5262372e039cff7fe3822003a858dfcd143ef0e
Michael Patel <michael.patel94@gmail.com>
2020-02-14 16:42:03 -0500

    Switching to camera2 api

--- app/src/main/java/com/example/cameramp/MainActivity.java
+++ app/src/main/java/com/example/cameramp/MainActivity.java
@@ -6,26 +6,38 @@ Use existing camera application
 
 ///////////////////////////////////////////////////////////////////////////
 // Imports
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 ///////////////////////////////////////////////////////////////////////////
 // MainActivity
 public class MainActivity extends AppCompatActivity {
 
     // class variable UI elements
-    Button takePictureButton;
-    ImageView imageView;
+    //Button takePictureButton;
+    //ImageView imageView;
+    TextureView textureView;
+
 
     // request codes
-    final int IMAGE_CAPTURE_REQUEST_CODE = 1;
+    //final int IMAGE_CAPTURE_REQUEST_CODE = 1;
 
     // onCreate()
     @Override
@@ -34,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); // inflate UI
 
         // initialize UI widgets to programmatically control later
-        takePictureButton = findViewById(R.id.takePictureButton);
-        imageView = findViewById(R.id.imageView);
+        //takePictureButton = findViewById(R.id.takePictureButton);
+        //imageView = findViewById(R.id.imageView);
 
         /*
         // button listener
@@ -50,15 +62,44 @@ public class MainActivity extends AppCompatActivity {
         });
          */
 
+        /*
         // have camera open automatically using default camera app
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(cameraIntent.resolveActivity(getPackageManager()) != null){  // prevent app from crashing
             startActivityForResult(cameraIntent, IMAGE_CAPTURE_REQUEST_CODE);
         }
+         */
 
         // create my own custom camera preview using SurfaceView
+        textureView = findViewById(R.id.textureView);
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
     }
 
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            // open camera
+            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // transform image
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            //
+        }
+    };
+
+    /*
     // onActivityResult() --> receive resultCode
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -73,5 +114,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+     */
 
 } // end class
